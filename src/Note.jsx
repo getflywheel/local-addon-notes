76894a5f32c94bb16848a2b3fcecf21dffce9be1
Jacopo Marrone <jacopo.marrone27@gmail.com>
2023-06-06 02:42:27 +0200

    feat: add edit note feature

--- src/Note.jsx
+++ src/Note.jsx
@@ -13,23 +13,28 @@ export default class Note extends Component {
 		pinned: PropTypes.bool,
 		onPin: PropTypes.func,
 		onDelete: PropTypes.func,
+		onEdit: PropTypes.func,
 	};
 
-	renderButtons () {
+	renderButtons() {
 
 		return <div className="NoteButtons">
 			<span className="Pin" onClick={this.props.onPin}>
-				<svg><use href={`file://${path.resolve(__filename, '../../assets/pin.svg')}#pin`}/></svg>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/pin.svg')}#pin`} /></svg>
+			</span>
+
+			<span className="Edit" onClick={this.props.onEdit}>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/edit.svg')}#edit`} /></svg>
 			</span>
 
 			<span className="Trash" onClick={this.props.onDelete}>
-				<svg><use href={`file://${path.resolve(__filename, '../../assets/trash.svg')}#trash`}/></svg>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/trash.svg')}#trash`} /></svg>
 			</span>
 		</div>;
 
 	}
 
-	render () {
+	render() {
 
 		return <InnerPaneSidebarContentItem className={classnames('Note', { '__Pinned': this.props.pinned })}>
 			<h5 className="Date hideOnSpectron">{dateFormat(this.props.date, 'mmmm dS, yyyy')}</h5>
