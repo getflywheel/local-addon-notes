172c98ecd42c252ba764981f405c6ffb24634957
joshuapregua <joshua.pregua@wpengine.com>
2025-03-25 00:47:19 +0800

    refactor(fix): replaced dateFormat into pure js date formatter

--- src/Note.jsx
+++ src/Note.jsx
@@ -5,69 +5,59 @@ import classnames from 'classnames';
 import path from 'path';
 
 export default class Note extends Component {
-    state = {
+	state = {
         formattedDate: null,
-        isLoading: true
     };
 
-    static propTypes = {
-        date: PropTypes.any,
-        body: PropTypes.string,
-        pinned: PropTypes.bool,
-        onPin: PropTypes.func,
-        onDelete: PropTypes.func,
-        onEdit: PropTypes.func,
-    };
+	static propTypes = {
+		date: PropTypes.any,
+		body: PropTypes.string,
+		pinned: PropTypes.bool,
+		onPin: PropTypes.func,
+		onDelete: PropTypes.func,
+		onEdit: PropTypes.func,
+	};
 
-    renderButtons() {
-        return (
-            <div className="NoteButtons">
-                <span className="Pin" onClick={this.props.onPin}>
-                    <svg key="pin"><use href={`file://${path.resolve(__filename, '../../assets/pin.svg')}#pin`} /></svg>
-                </span>
-                <span className="Edit" onClick={this.props.onEdit}>
-                    <svg key="edit"><use href={`file://${path.resolve(__filename, '../../assets/edit.svg')}#edit`} /></svg>
-                </span>
-                <span className="Trash" onClick={this.props.onDelete}>
-                    <svg key="trash"><use href={`file://${path.resolve(__filename, '../../assets/trash.svg')}#trash`} /></svg>
-                </span>
-            </div>
-        );
+	async componentDidMount() {
+        const date = new Date(this.props.date);
+		this.setState({
+			formattedDate: date.toLocaleDateString('en-US', {
+				month: 'long',
+				day: 'numeric',
+				year: 'numeric'
+			}),
+			isLoading: false
+		});
     }
 
-    async componentDidMount() {
-        try {
-            const dateFormat = await import('dateformat');
-            const dateObj = new Date(this.props.date);
-            this.setState({
-                formattedDate: dateFormat.default(dateObj, 'mmmm d, yyyy'),
-                isLoading: false
-            });
-        } catch (error) {
-            console.error('Failed to load dateformat:', error);
-            const dateObj = new Date(this.props.date);
-            this.setState({
-                formattedDate: dateObj.toLocaleDateString('en-US', {
-                    month: 'long',
-                    day: 'numeric',
-                    year: 'numeric'
-                }),
-                isLoading: false
-            });
-        }
-    }
+	renderButtons() {
 
-    render() {
-        const { formattedDate, isLoading } = this.state;
+		return <div className="NoteButtons">
+			<span className="Pin" onClick={this.props.onPin}>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/pin.svg')}#pin`} /></svg>
+			</span>
 
-        return (
-            <InnerPaneSidebarContentItem className={classnames('Note', { '__Pinned': this.props.pinned })}>
-                <h5 className="Date hideOnSpectron">
-                    {isLoading ? 'Loading...' : formattedDate}
-                </h5>
-                {this.renderButtons()}
-                <Markdown src={this.props.body} />
-            </InnerPaneSidebarContentItem>
-        );
-    }
+			<span className="Edit" onClick={this.props.onEdit}>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/edit.svg')}#edit`} /></svg>
+			</span>
+
+			<span className="Trash" onClick={this.props.onDelete}>
+				<svg><use href={`file://${path.resolve(__filename, '../../assets/trash.svg')}#trash`} /></svg>
+			</span>
+		</div>;
+
+	}
+
+	render() {
+		const { formattedDate } = this.state
+		return <InnerPaneSidebarContentItem className={classnames('Note', { '__Pinned': this.props.pinned })}>
+			<h5 className="Date hideOnSpectron">{ formattedDate }</h5>
+
+			{this.renderButtons()}
+
+			<Markdown src={this.props.body} />
+		</InnerPaneSidebarContentItem>;
+
+	}
+
 }
